7ba5ebfa581627941fe36f55c122eb58786e1518
Lulu <luluwheelan@gmail.com>
2019-04-10 11:47:54 -0400

    Working on functions.

--- javascript.js
+++ javascript.js
@@ -1,68 +1,101 @@
-
+// Select elements
 const scoreEle = document.querySelector('#score');
+const monsterLevel = document.querySelector('#monsterLevel');
+const start = document.querySelector('#start');
 const question = document.querySelector('#question');
-const answer = document.querySelector('#answer');
+const feedback = document.querySelector('#feedback');
+const inputAnswer = document.querySelector('#inputAnswer');
 const rightAnswer = document.querySelector('#rightAnswer');
-const hint = document.querySelector('#hint');
+const skipQuestion = document.querySelector('#skipQuestion');
+const submit = document.querySelector('#submit');
 
+// Set variables
 var score = 0;
+var playing = false;
 
+//click to start a game
+start.onclick = function(){
+	playing = true;
+     newGame = new MathGame();
 
-function evaluateAnswer(){ // Begin evaluateGuess
-  // a) Compare the guess to the current number. If the guess is correct, return true, otherwise return false.
-    return rightAnswer === Number(answer.value);
-  
+    newGame.Addition();
+
+    submit.onclick = newGame.processGame;
+   // feedback.textContent = Number(rightAnswer.value);
+
+   
 }
 
 
-function startAddition() {
-    addition();
+var MathGame = function(options = {}) {
+	// this.score = 0;
+	// this.playing = true;
+
+
+
+};
+
+MathGame.prototype.generateQuestion = function(){
+	var allFuns = ['Addition','Subtraction','tMultiplication', 'Division'];
+	var i = Math.floor(Math.random()*allFuns.length);
+
+	allFuns[i]();
+
 }
-
-function startSubtraction() {
-    subtraction();
-}
-
-function startMultiplication() {
-    multiplication();
-}
-
-function startDivision() {
-    division();
-}
-
-
-function addition() {
-    let num1 = Math.floor(Math.random() *100);
+MathGame.prototype.Addition = function(){
+	let num1 = Math.floor(Math.random() *100);
     let num2 = Math.floor(Math.random() *(100-num1));
     
  question.textContent =  num1 + " + " + num2 + " =";
  rightAnswer.value = (num1 + num2);
+
 }
 
-function subtraction() {
-    let num1 = Math.floor(Math.random() *100);
+MathGame.prototype.Subtraction = function(){
+	let num1 = Math.floor(Math.random() *100);
     let num2 = Math.floor(Math.random() *num1);
     
  question.textContent =  num1 + " - " + num2 + " =";
  rightAnswer.value = (num1 + num2);
 }
 
-
-function multiplication() {
-    let num1 = Math.floor(Math.random() *10+1);
+MathGame.prototype.Multiplication = function(){
+	let num1 = Math.floor(Math.random() *10+1);
     let num2 = Math.floor(Math.random() *10+1);
     
  question.textContent =  num1 + " * " + num2 + " =";
  rightAnswer.value = (num1 + num2);
 }
 
-function division() {
-
-    let num1 = Math.floor(Math.random() *10+1);
+MathGame.prototype.Division = function(){
+	let num1 = Math.floor(Math.random() *10+1);
     let num2 = Math.floor(Math.random() *10+1);
     let answer = num1 * num2;
     
  question.textContent =  answer + " / " + num1 + " =";
  rightAnswer.value = num2;
+}
+
+
+MathGame.prototype.processGame = function(){
+
+	if (this.evaluateAns {//if answer is right
+
+		playing = true;
+		score++;
+		feedback.textContent = "You did great!";
+
+	}else{
+
+		score--;
+		feedback.textContent = "Try again or skip the queation."
+		//if(skipQuestion.onclick() -> this.Addition)
+	}
+	scoreEle.textContent = score;
+
+}
+
+MathGame.prototype.evaluateAns = function(){ 
+	answer = Number(rightAnswer.value);
+	return answer === Number(inputAnswer.value);
 }
