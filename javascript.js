604bed7d273b77000f1150b416a67378f4b14bd8
Yingge Hu <ayanamirei629@gmail.com>
2019-04-11 19:58:01 -0400

    onclick

--- javascript.js
+++ javascript.js
@@ -196,3 +196,12 @@ monster.onclick = function(){
 }
 
 
+inputAnswer.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    submit.click();
+  }
+});
